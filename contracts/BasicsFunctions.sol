4cc8b024ac334c55fc2d0d712d9d56d82444eb14
willowsenator <willowsenator@gmail.com>
2023-04-25 15:35:31 +0200

    More functions

--- contracts/BasicsFunctions.sol
+++ contracts/BasicsFunctions.sol
@@ -3,6 +3,9 @@
 pragma solidity >=0.7.0 < 0.9.0;
 
 contract BasicsFunctions{
+    string coinName = "MY COIN";
+    uint public myBalance = 1000;
+
     struct Coin{ 
         string name;
         string symbol;
@@ -16,5 +19,13 @@ contract BasicsFunctions{
         else{
             return false;
         }
+    }
+
+    function getMyCoin() public view returns (string memory){
+        return coinName;
+    }
+
+    function multiply(uint _factor) external {
+        myBalance *= _factor;
     }
 }
